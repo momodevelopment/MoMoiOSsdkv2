6afeef47c1ca9fc89b223b521da84e118ec11075
momodevelopment <lanhluu.vn@gmail.com>
2017-11-24 21:33:17 +0700

    Initial commit

---
+++ Example/MoMoiOSsdkv2/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  MoMoiOSsdkv2
+//
+//  Created by momodevelopment on 11/24/2017.
+//  Copyright (c) 2017 momodevelopment. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TestViewController : UIViewController
+
+@end
---
+++ Example/MoMoiOSsdkv2/TestViewController.m
@@ -0,0 +1,29 @@
+//
+//  TestViewController.m
+//  MoMoiOSsdkv2
+//
+//  Created by momodevelopment on 11/24/2017.
+//  Copyright (c) 2017 momodevelopment. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
